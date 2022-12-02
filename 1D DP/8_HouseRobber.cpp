b3e4bec152f8ce9d5176da65e504a005d5bfb087
24Shreyaskumar <shreyaskumar.12010414@lpu.in>
2022-12-02 20:13:17 +0530

    commit on master

--- 1D DP/8_HouseRobber.cpp
+++ 1D DP/8_HouseRobber.cpp
@@ -25,6 +25,11 @@ int main() {
     vi v(n);
     for (int i = 0; i < n; i++) cin>>v[i];
 
+    if (n == 1) {
+        cout<<v[0]<<"\n";
+        return 0;
+    } 
+
     vi vf(n-1), vl(n-1);
     for (int i = 0; i < n; i++) {
         if (i < n-1) vf[i] = v[i];
--- 2D DP/1_NinjaTraining.cpp
+++ 2D DP/1_NinjaTraining.cpp
@@ -3,7 +3,7 @@
 #define vvi vector<vector<int>>
 using namespace std;
 
-int Training(int day, int memory, vvi v, vvi &dp) {
+int Training_memo(int day, int memory, vvi v, vvi &dp) {
     if (day == 0) {
         int mx = 0;
         for (int i = 0; i < 3; i++) {
@@ -17,12 +17,35 @@ int Training(int day, int memory, vvi v, vvi &dp) {
     int mx = 0;
     for (int i = 0; i < 3; i++) {
         if (i != memory) {
-            int points = v[day][i] + Training(day-1, i, v, dp);
+            int points = v[day][i] + Training_memo(day-1, i, v, dp);
             mx = max(mx, points);
         }
     }
 
     return dp[day][memory] = mx;
+}
+
+int Training_tab(int days, int memory, vvi v, vvi &dp) {
+    int mx = 0;
+    dp[0][0] = max(v[0][1], v[0][2]);
+    dp[0][1] = max(v[0][0], v[0][2]);
+    dp[0][2] = max(v[0][0], v[0][1]);
+    dp[0][3] = max(v[0][0], max(v[0][1], v[0][2]));
+
+    for (int i = 1; i < days; i++) {
+        for (int memory = 0; memory <= 3; memory++) {
+            dp[i][memory] = 0;
+
+            for (int j = 0; j < 3; j++) {
+                if (j != memory) {
+                    int points = v[i][j] + dp[i-1][j];
+                    dp[i][memory] = max(dp[i][memory], points);
+                }
+            }
+        }
+    }
+
+    return dp[days-1][3];
 }
 
 int main() {
@@ -40,7 +63,8 @@ int main() {
             for (int j = 0; j < 3; j++) cin>>v[i][j];
         }
 
-        cout<<Training(days-1, 3, v, dp)<<"\n";
+        cout<<"Memoization : "<<Training_memo(days-1, 3, v, dp)<<"\n";
+        cout<<"Tabulation : "<<Training_tab(days-1, 3, v, dp)<<"\n";
     }
 
     return 0;
